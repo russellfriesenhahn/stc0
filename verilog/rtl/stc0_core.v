c4ee1937bcfe772cd9414a1c6c3d303869a59ba0
Russell L Friesenhahn <russellf@utexas.edu>
2021-06-13 23:10:00 -0500

    Add power pin ports

--- verilog/rtl/stc0_core.v
+++ verilog/rtl/stc0_core.v
@@ -10,6 +10,16 @@ module stc0_core#(
     parameter NUM_POINTS_LOG2 = 10,
     parameter TW_WIDTH = 16
 ) (
+`ifdef USE_POWER_PINS
+    inout vdda1,	// User area 1 3.3V supply
+    inout vdda2,	// User area 2 3.3V supply
+    inout vssa1,	// User area 1 analog ground
+    inout vssa2,	// User area 2 analog ground
+    inout vccd1,	// User area 1 1.8V supply
+    inout vccd2,	// User area 2 1.8v supply
+    inout vssd1,	// User area 1 digital ground
+    inout vssd2,	// User area 2 digital ground
+`endif
     input wire          ClkIngress,
     input wire          ClkProc,
     input wire          ARst,
